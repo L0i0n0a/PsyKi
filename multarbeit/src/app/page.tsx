7c333c8fd511997a70e40103289dad1910b2cb5e
Fabian <f.nagorny@mailbox.org>
2025-05-09 10:21:08 +0200

    prepare site for deployment

---
+++ multarbeit/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <h1>Multimediale Ausarbeitung</h1>
+                <h2>Strategie: Optimale Gewichtung</h2>
+            </div>
+        </div>
+    );
+}
