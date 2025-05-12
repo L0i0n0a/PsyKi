0c0d6e41d0d40c243cd18735826f4492559f64ab
Angelina <angelina.grauerholz@student.uni-luebeck.de>
2025-05-12 09:37:11 +0200

    fix: image urls

--- multarbeit/src/app/page.tsx
+++ multarbeit/src/app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
             </div>
             {/*  <Carousel images={["../../file.svg", "../../globe.svg"]} /> */}
             <SectionRight imageSource="Benchmarking Aided Decision Making in a Signal Detection Task
-Megan L. Bartlett and Jason S. McCarley, Human Factors 2017 59:6, 881-900" onClick={() => router.push("/details/theory")} imageUrl="../../graph.png" title="Optimale Gewichtung: Theorie " description={
+Megan L. Bartlett and Jason S. McCarley, Human Factors 2017 59:6, 881-900" onClick={() => router.push("/details/theory")} imageUrl="/graph.png" title="Optimale Gewichtung: Theorie " description={
                     <>
                         Das Optimal Weighting (OW) Modell ist ein <strong>theoretisches Modell</strong>, das beschreibt, wie ein Mensch oder ein System die Hinweise von einem <strong>menschlichen Entscheider</strong> und eines <strong>automatisierten Hilfsmittels</strong> optimal kombiniert, um die <strong>beste mögliche Entscheidung</strong> zu treffen.
                     </>
                 } />
-            <SectionLeft imageSource="Med Badr Chemmaoui (Unsplash)" onClick={() => router.push("/details/design/1")} imageUrl="../..unsplash_med.jpg" title="Erste Designentscheidungen" description={
+            <SectionLeft imageSource="Med Badr Chemmaoui (Unsplash)" onClick={() => router.push("/details/design/1")} imageUrl="/unsplash_med.jpg" title="Erste Designentscheidungen" description={
                 <>
                     Unsere Anwendung richtet sich an <strong>Berufsgruppen mit hoher Verantwortung</strong> in der <strong>visuellen Entscheidungsfindung</strong> (z. B. Radiologie, Qualitätssicherung, Meteorologie), bei denen <strong>Genauigkeit</strong> und <strong>Verlässlichkeit</strong> entscheidend sind. Basierend auf dem Optimal Weighting Modell kombinieren wir objektive Leistungsdaten (z. B. Sensitivität aus einem Vor-Test) mit subjektiver Sicherheit, um menschliche und KI-basierte Hinweise <strong>optimal zu gewichten</strong>. Ziel ist eine <strong>adaptive Entscheidungsunterstützung</strong>, die Fehler reduziert, Transparenz schafft und dem Nutzer trotz KI-Empfehlung die <strong>Kontrolle</strong> lässt. Die <strong>Nutzerführung</strong> umfasst einen kurzen <strong>Baseline-Test</strong> und einen Hauptdurchgang mit erklärter Entscheidungsunterstützung.
                 </>
