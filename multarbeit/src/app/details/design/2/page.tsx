34be56af298a3d30272148cf6cce274037b00160
Angelina <angelina.grauerholz@student.uni-luebeck.de>
2025-05-27 15:05:32 +0200

    feat: added new sections for documentation

--- multarbeit/src/app/details/design/2/page.tsx
+++ multarbeit/src/app/details/design/2/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import Button from "@/components/ui/Button/Button";
 import BiColor from "@/components/canvas/BiColor";
+import Image from "next/image";
 
 const DesignDecisionsPage2 = () => {
 
@@ -12,8 +13,25 @@ const DesignDecisionsPage2 = () => {
             <div className="header border10">
                 <h1 className="text-4xl font-bold m-4 text-center">Prototyp-Entwurf & Userflows</h1>
             </div>
+            
+            <section className="sectionBorder">
+                <h2 className="text-2xl font-semibold">User Flows</h2>
+                <h2 className="text-2xl mb-4">Wie navigieren Nutzende durch die Anwendung?</h2>
+                
+            </section>
 
-            <BiColor/>
+            <section className="sectionBorder">
+                <h2 className="text-2xl font-semibold">Voruberelegungen zum Prototyp</h2>
+                <h2 className="text-2xl mb-4">Welchen Umfang soll der Prototyp haben?</h2>
+                
+            </section>
+
+            <section className="sectionBorder">
+                <h2 className="text-2xl font-semibold">Umsetzungsueberlegungen fuer die Anwendung</h2>
+                <h2 className="text-2xl mb-4">Wie koennen die Bilder erstellt werden?</h2>
+                <BiColor />
+            </section>
+            
 
 
             <div className="flex justify-center"><Button text="Zurück" onClick={() => router.push("/")} /></div>
